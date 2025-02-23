5b8bd840c1966b2744bfed8391ba8fe24e33f7c0
cukJAa <jurgencukaj@gmail.com>
2025-02-23 19:00:31 +0100

    nami nami

--- event_management/event/views.py
+++ event_management/event/views.py
@@ -8,18 +8,6 @@ from ticket.models import Ticket
 from datetime import datetime
 
 
-def event_list(request):
-    events = Event.objects.all()
-    return render(request, 'events.html', {'events': events})
-def event_list(request):
-    """Display all events (attendees see all, organizers see only their own)."""
-    if request.user.is_authenticated and request.user.type == "organizer":
-        events = Event.objects.filter(organizer=request.user)
-    else:
-        events = Event.objects.all()
-    
-    return render(request, "events.html", {"events": events})
-
 def performer(request):
     events = Event.objects.all()
     return render(request, 'events.html', {'events': events})
@@ -41,74 +29,9 @@ def program(request):
 def index(request):
     return render( request, 'index.html')
 
-@login_required
-@permission_required("event_management.add_event", raise_exception=True)
-def event_create(request):
-    """Allow organizers to create an event."""
-    if request.user.type != "organizer":
-        messages.error(request, "Only organizers can create events.")
-        return redirect("events")
-
-    if request.method == "POST":
-        form = EventForm(request.POST)
-        if form.is_valid():
-            event = form.save(commit=False)
-            event.organizer = request.user  # Assign event to logged-in organizer
-            event.save()
-            messages.success(request, "Event created successfully!")
-            return redirect("events")
-    else:
-        form = EventForm()
-    
-    return render(request, "event_create.html", {"form": form})
-
-@login_required
-@permission_required("event_management.change_event", raise_exception=True)
-def event_update(request, event_id):
-    """Allow an organizer to edit their own event."""
-    event = get_object_or_404(Event, id=event_id)
-
-    if request.user != event.organizer:
-        messages.error(request, "You can only edit your own events.")
-        return redirect("events")
-
-    if request.method == "POST":
-        form = EventForm(request.POST, instance=event)
-        if form.is_valid():
-            form.save()
-            messages.success(request, "Event updated successfully!")
-            return redirect("events")
-    else:
-        form = EventForm(instance=event)
-    
-    return render(request, "event_create.html", {"form": form})
-
-@login_required
-@permission_required("event_management.delete_event", raise_exception=True)
-def event_delete(request, event_id):
-    """Allow an organizer to delete their own event."""
-    event = get_object_or_404(Event, id=event_id)
-
-    if request.user != event.organizer:
-        messages.error(request, "You can only delete your own events.")
-        return redirect("events")
-
-    event.delete()
-    messages.success(request, "Event deleted successfully!")
-    return redirect("events")
-
 def ticket_details(request, event_id):
     event = get_object_or_404(Event, id=event_id)
     return render(request, 'ticket_details.html', {'event': event})
-
-def contact(request):
-    return render(request, 'contact.html')
-
-def about(request):
-    return render(request, 'about.html')
-
-def index(request):
-    return render(request, 'index.html')
 
 def home(request):
     events = Event.objects.all()
@@ -144,35 +67,36 @@ def event_create(request):
     if request.user.type != "organizer":
         messages.error(request, "Only organizers can create events.")
         return redirect("event_list")
+    
+    locations = Location.objects.all()
 
     if request.method == "POST":
-        form = EventForm(request.POST, request.FILES)
-        if form.is_valid():
-            event = form.save(commit=False)
+        event_form = EventForm(request.POST, request.FILES)
+        ticket_forms = [TicketForm(request.POST, prefix=str(i)) for i in range(3)]  # Assuming 3 ticket categories
+
+        if event_form.is_valid() and all([tf.is_valid() for tf in ticket_forms]):
+            event = event_form.save(commit=False)
             event.organizer = request.user  # Assign event to logged-in organizer
             event.save()
-            messages.success(request, "Event created successfully!")
-            return redirect(reverse("organizer_event_list"))
-    else:
-        form = EventForm()
-        locations = Location.objects.all()
-
-    ticket_forms = [TicketForm(request.POST, prefix=str(i)) for i in range(3)]  # Assuming 3 ticket categories
-
-    if event_form.is_valid() and all([tf.is_valid() for tf in ticket_forms]):
-            event = event_form.save()
             for tf in ticket_forms:
                 ticket = tf.save(commit=False)
                 ticket.event = event
                 ticket.save()
             messages.success(request, "Event and tickets created successfully!")
-            return redirect('events')
+            return redirect(reverse("organizer_event_list"))
+        else:
+            print("Event form errors:", event_form.errors)  # Debugging
+            for tf in ticket_forms:
+                print("Ticket form errors:", tf.errors)  # Debugging
+            messages.error(request, "There was an error in the form. Please fix it.")
+
     else:
         event_form = EventForm()
-        ticket_forms = [TicketForm(prefix=str(i)) for i in range(3)]
-        
-    
-    return render(request, "create_event.html", {"form": form, "locations": locations, 'ticket_forms': ticket_forms} )
+        ticket_forms = [TicketForm(prefix=str(i)) for i in range(3)]  # Assuming 3 ticket categories
+
+    return render(request, "create_event.html", {"form": event_form, "locations": locations, 'ticket_forms': ticket_forms})
+
+
 
 @login_required
 # @permission_required("event_management.change_event", raise_exception=True)
@@ -182,19 +106,18 @@ def event_update(request, event_id):
 
     if request.user != event.organizer:
         messages.error(request, "You can only edit your own events.")
-        return redirect("organizer_event_list")
+        return redirect("events")
 
     if request.method == "POST":
         form = EventForm(request.POST, instance=event)
         if form.is_valid():
             form.save()
             messages.success(request, "Event updated successfully!")
-            return redirect("organizer_event_list")
+            return redirect("events")
     else:
         form = EventForm(instance=event)
-
-    locations = Location.objects.all()
-    return render(request, "update_event.html", {"form": form, "event": event, "locations": locations})
+    
+    return render(request, "event_create.html", {"form": form})
 
 @login_required
 # @permission_required("event_management.delete_event", raise_exception=True)
