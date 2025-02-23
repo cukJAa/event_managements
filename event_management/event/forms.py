3c267b2ace4e20bae9545880372ef1342b0a40fa
cukJAa <jurgencukaj@gmail.com>
2025-02-23 15:55:57 +0100

    resolved forms conflict

--- event_management/event/forms.py
+++ event_management/event/forms.py
@@ -1,7 +1,13 @@
 from django import forms
 from .models import Event
+from ticket.models import Ticket
 
 class EventForm(forms.ModelForm):
     class Meta:
         model = Event
         fields = ["name", "description", "start_time", "end_time", "location", "image"]
+
+class TicketForm(forms.ModelForm):
+    class Meta:
+        model = Ticket
+        fields = ['category', 'price', 'available_quantity']
